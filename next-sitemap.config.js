1a05877aa8dd124a76b779877ec93444c8d43c9a
FlatBushZombies <brianmakanjira18@gmail.com>
2025-11-20 17:42:13 +0200

    add sitemap xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://noirsfera.com', 
+  generateRobotsTxt: true,           
+  sitemapSize: 5000,                  
+  changefreq: 'weekly',               
+  priority: 0.7,                      
+  exclude: ['/secret-page'],          
+  
+
+}
